9fb06545c0ec7ed5b623c4198e504123b1f83715
Giovane <giovane.barreira@arctouch.com>
2023-02-04 10:38:07 -0300

    Initial Commit

---
+++ ImgurCats/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ImgurCats
+//
+//  Created by Giovane Barreira on 04/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
